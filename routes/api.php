a67c666e2df2f8eede6b452ba4afd3bba01e9414
root <root@DESKTOP-9HMQGU5.localdomain>
2019-08-10 00:53:01 -0500

    ok

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Http\Request;
 
     Route::post('registerUser', 'Api\RegisterController@registerUser');
     Route::get('listProductUnit', 'Api\Inventory\ProductUnitController@listProductUnit');
+    Route::post('deleteProductUnit', 'Api\Inventory\ProductUnitController@deleteProductUnit');
